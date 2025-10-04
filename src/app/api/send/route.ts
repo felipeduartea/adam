433de2b7551c50d27684fb2eee8d46d823835389
enzoribeiroz <enzoribeiro.2005@gmail.com>
2025-10-04 12:25:50 -0700

    fix: resend api

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,25 +1,33 @@
-import { EmailTemplate } from "../../../components/email-template";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+type EmailRequestProps = {
+  to: string;
+  subject?: string;
+  text: string;
+};
+
+export async function POST(req: Request) {
   try {
+    const body: EmailRequestProps = await req.json();
+    const { to, subject, text } = body;
+
     const { data, error } = await resend.emails.send({
-      from: "Enzo <enzo@formcn.ai>",
-      to: ["enzoribeiroz@berkeley.edu"],
-      subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }),
+      from: "Adam <adam@formcn.ai>",
+      to: [to],
+      subject: subject || "Hello world",
+      react: text,
     });
 
     if (error) {
-      console.log(error);
+      console.error(error);
       return Response.json({ error }, { status: 500 });
     }
 
     return Response.json(data);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return Response.json({ error }, { status: 500 });
   }
 }
