700e631130335049c5c44a41e993c6b97917ef9c
enzoribeiroz <enzoribeiro.2005@gmail.com>
2025-10-04 12:00:04 -0700

    feat: resend email template

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,25 @@
+import { EmailTemplate } from "../../../components/email-template";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Enzo <enzo@formcn.ai>",
+      to: ["enzoribeiroz@berkeley.edu"],
+      subject: "Hello world",
+      react: EmailTemplate({ firstName: "John" }),
+    });
+
+    if (error) {
+      console.log(error);
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error }, { status: 500 });
+  }
+}
